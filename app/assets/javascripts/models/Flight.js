288da551477ff51878eb2254d34a0f1263e63604
katiepetersonn <katie.peterson19@gmail.com>
2017-05-18 09:35:29 +1000

    pages controller

--- app/assets/javascripts/models/Flight.js
+++ app/assets/javascripts/models/Flight.js
@@ -4,11 +4,11 @@ app.Flight = Backbone.Model.extend({
     urlRoot: "/flights",
 
     defaults : {
-        flight_number: "0",
-        origin: "No origin",
-        destination: "No desitationi",
-        flight_date: Date.now(),
-        airplane_id: 0
+        // flight_number: "0",
+        // origin: "No origin",
+        // destination: "No desitationi",
+        // flight_date: Date.now(),
+        // airplane_id: 0
     },
 
     initialize: function() {

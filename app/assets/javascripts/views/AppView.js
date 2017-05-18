c32afd9380f10f8497b93f87552119daedab4191
Philg112 <phil.grujovski@gmail.com>
2017-05-18 12:16:27 +1000

    models now showing on page

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -6,6 +6,11 @@ app.AppView = Backbone.View.extend({
   render: function () {
     var templatemarkup = $("#SearchViewTemplate").html();
     this.$el.html(templatemarkup);
-    
+    this.collection.each(function(flight) {
+      sv = new app.SearchView({
+        model: flight
+      });
+      sv.render();
+    });
   }
 });
